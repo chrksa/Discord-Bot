b589a6a197eadbdff1d999807426bdacf0982728
Chris <christopher.liebsch@st.ovgu.de>
2023-02-11 16:18:08 +0100

    changed direction of .env and succesfully connected to discord

--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@ import os
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path='/home/chris/DiscordBot/.env')
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-        print(f'{client.user} has connected to Discord!')
+        print(f'{client.user} has connected to Discord! WUHU')
 
 client.run(TOKEN)
