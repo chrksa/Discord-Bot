9d78d4820e1a10aa2ceb8292f741513197fcff5a
Chris <christopher.liebsch@st.ovgu.de>
2023-02-11 16:04:09 +0100

    bot.py added Client Intents

--- bot.py
+++ bot.py
@@ -1,20 +1,14 @@
 #bot.py 
 import os
-
 import discord
-
-from dotenv import load_dotenv
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.client()
+client = discord.Client(intents=discord.Intents.default())
 
 @client.event
-
 async def on_ready():
         print(f'{client.user} has connected to Discord!')
 
